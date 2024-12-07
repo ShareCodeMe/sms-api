86ab1048cfea68aff1d62f7eb574087693d12089
“nhaserey” <panhaserey80@gmail.com>
2024-12-07 11:40:53 +0700

    add auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,8 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PrismaModule } from './prisma';
-import { AuthModule } from './auth/auth.module';
+import { AuthModule } from './auth';
+
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule],
