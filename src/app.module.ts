e90b5ebf286e65c083e4350c583c9d16a8d9fba1
“nhaserey” <panhaserey80@gmail.com>
2024-12-07 11:46:45 +0700

    U

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
+import { UsersModule } from './users';
 import { PrismaModule } from './prisma';
 import { AuthModule } from './auth';
-
 
 @Module({
   imports: [UsersModule, PrismaModule, AuthModule],
